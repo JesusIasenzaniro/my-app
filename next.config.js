260215e1e70490b95fa2623031abf91b7f591e6e
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-01-11 14:51:15 +0100

    config experimental & the images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    experimental: {
+        appDir: true,
+    },
+    reactStrictMode: true,
+    images: {
+        domains: ['v5.airtableusercontent.com'],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
