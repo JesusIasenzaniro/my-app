e49688ec43aa584270237b09288f79b0e7e6d1f7
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-01-11 14:53:39 +0100

    add product type

---
+++ typings.d.ts
@@ -0,0 +1,10 @@
+export type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
+    colors: string[];
+    company: string;
+    description: string;
+    category: string;
+};
