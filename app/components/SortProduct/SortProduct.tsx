6d606560a2456662f0b8507f36f310f5a352c98d
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2023-01-12 14:10:02 +0100

    sort refactor

--- app/components/SortProduct/SortProduct.tsx
+++ app/components/SortProduct/SortProduct.tsx
@@ -6,22 +6,20 @@ const SortProduct = () => {
     const value = useContext(AppContext);
 
     return (
-        <article className={styles.sortContainer}>
-            <form>
-                <label htmlFor='sort'>Sort by </label>
-                <select
-                    name='sort'
-                    id='sort'
-                    className='sortInput'
-                    value={value?.sort}
-                    onChange={(e) => value?.setSort(e.target.value)}
-                >
-                    <option value='all'>all</option>
-                    <option value='price-lowest'>price (lowest)</option>
-                    <option value='price-highest'>price (highest)</option>
-                </select>
-            </form>
-        </article>
+        <form className={styles.sortContainer}>
+            <label htmlFor='sort'>Sort by:</label>
+            <select
+                name='sort'
+                id='sort'
+                className='sortInput'
+                value={value?.sort}
+                onChange={(e) => value?.setSort(e.target.value)}
+            >
+                <option value='all'>all</option>
+                <option value='price-lowest'>price (lowest)</option>
+                <option value='price-highest'>price (highest)</option>
+            </select>
+        </form>
     );
 };
 
